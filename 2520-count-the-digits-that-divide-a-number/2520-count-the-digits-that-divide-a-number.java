f6f11c3997cb3fdff77c77322cbc3459988af792
AYA RAZA <123098anan@gmail.com>
2026-09-09 11:52:24 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.66%) - LeetHub-Neo

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countDigits(int num) {
+        int temp = num;
+        int cnt = 0;
+        while(temp>0){
+            if(num % ( temp % 10) ==0){
+                cnt++;
+            }
+            temp = temp/10;
+        }
+
+        return cnt;
+    } 
+}
